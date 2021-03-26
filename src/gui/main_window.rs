804e301c1bc4d15cbe85fe58000413aea2efc059
Marius Wilms <wilms.marius95@gmail.com>
2021-03-26 23:56:22 +0100

    Added more to the dummy frontend

--- src/gui/main_window.rs
+++ src/gui/main_window.rs
@@ -1,4 +1,7 @@
-use iced::{button, text_input, Align, Button, Element, Row, Sandbox, Settings, Text, TextInput};
+use iced::{
+    button, text_input, Align, Button, Column, Container, Element, Length, Row, Sandbox, Settings,
+    Text, TextInput,
+};
 
 #[derive(Default)]
 pub struct MainWindow {
@@ -32,8 +35,6 @@ impl Sandbox for MainWindow {
     }
 
     fn view(&mut self) -> Element<Message> {
-        let top_panel = Row::new();
-
         let refresh_button = Button::new(&mut self.refresh_button, Text::new("Refresh"))
             .on_press(Message::RefreshPressed);
         let update_all_button = Button::new(&mut self.update_all_button, Text::new("Update all"))
@@ -46,11 +47,38 @@ impl Sandbox for MainWindow {
             Message::InputChanged,
         );
 
-        top_panel
+        let plugin_name = Text::new("Plugin");
+        let current_version = Text::new("Current Version");
+        let latest_version = Text::new("Latest version");
+        let upgrade = Text::new("Upgrade");
+
+        let header_column = Row::new()
+            .width(Length::Fill)
+            .align_items(Align::Center)
+            .spacing(10)
+            .push(plugin_name)
+            .push(current_version)
+            .push(latest_version)
+            .push(upgrade);
+
+        let top_panel = Row::new()
+            .width(Length::Fill)
+            .align_items(Align::Center)
+            .spacing(10)
             .push(refresh_button)
             .push(update_all_button)
             .push(installed_plugins)
-            .push(search_plugins)
+            .push(search_plugins);
+
+        let content = Column::new()
+            .width(Length::Fill)
+            .align_items(Align::Center)
+            .push(top_panel)
+            .push(header_column);
+
+        Container::new(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
             .into()
     }
 
