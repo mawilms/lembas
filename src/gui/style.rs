43062ba5c28d16b1f534e1c347977b2d04746f02
Marius Wilms <wilms.marius95@gmail.com>
2021-03-27 17:16:23 +0100

    Added constants

--- src/gui/style.rs
+++ src/gui/style.rs
@@ -1,5 +1,12 @@
 use iced::{button, container, Background, Color, Vector};
 
+pub const BORDER_COLOR: Color = Color::from_rgb(0.54, 0.53, 0.31);
+pub const BUTTON_COLOR_DEFAULT: Color = Color::from_rgb(0.60, 0.59, 0.32);
+pub const BUTTON_COLOR_HOVER: Color = Color::from_rgb(0.25, 0.18, 0.13);
+pub const BACKGROUND_COLOR: Color = Color::from_rgb(0.84, 0.71, 0.49);
+pub const COLUMN_COLOR_PRIMARY: Color = Color::from_rgb(0.25, 0.18, 0.13);
+pub const COLUMN_COLOR_SECONDARY: Color = Color::from_rgb(0.25, 0.18, 0.13);
+
 pub enum PrimaryButton {
     Enabled,
     Disabled,
@@ -9,8 +16,8 @@ impl button::StyleSheet for PrimaryButton {
     fn active(&self) -> button::Style {
         match self {
             Self::Enabled => button::Style {
-                background: Some(Background::Color(Color::from_rgb(0.35, 0.27, 0.18))),
-                border_color: Color::from_rgb(0.25, 0.18, 0.13),
+                background: Some(Background::Color(BUTTON_COLOR_DEFAULT)),
+                border_color: BORDER_COLOR,
                 border_width: 4.0,
                 shadow_offset: Vector::new(1.0, 1.0),
                 text_color: Color::from_rgb8(0xEE, 0xEE, 0xEE),
@@ -49,7 +56,7 @@ pub struct Content;
 impl container::StyleSheet for Content {
     fn style(&self) -> container::Style {
         container::Style {
-            background: Some(Background::Color(Color::from_rgb(0.72, 0.61, 0.44))),
+            background: Some(Background::Color(BACKGROUND_COLOR)),
             text_color: Some(Color::WHITE),
             ..container::Style::default()
         }
