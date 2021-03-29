a3984bd8a7375e4f2d8feb9c162f4230c7d98139
Marius Wilms <wilms.marius95@gmail.com>
2021-03-29 23:47:46 +0200

    Added dynamic population for plugins

--- src/gui/elements/plugin_panel.rs
+++ src/gui/elements/plugin_panel.rs
@@ -13,41 +13,14 @@ impl PluginPanel {
         let latest_version = Text::new("Latest version").width(Length::FillPortion(3));
         let upgrade = Text::new("Upgrade").width(Length::FillPortion(2));
 
-        let header = Row::new()
+        Row::new()
             .width(Length::Fill)
             .align_items(Align::Center)
             .spacing(10)
             .push(plugin_name)
             .push(current_version)
             .push(latest_version)
-            .push(upgrade);
-
-        let plugin_name = Text::new("TitanBar").width(Length::FillPortion(5));
-        let current_version = Text::new("v1.20.00").width(Length::FillPortion(3));
-        let latest_version = Text::new("v1.24.45").width(Length::FillPortion(3));
-        let upgrade = Text::new("Upgrade").width(Length::FillPortion(2));
-
-        let item = Row::new()
-            .width(Length::Fill)
-            .align_items(Align::Center)
-            .spacing(10)
-            .push(plugin_name)
-            .push(current_version)
-            .push(latest_version)
-            .push(upgrade);
-
-        let content = Column::new()
-            .width(Length::Fill)
-            .spacing(10)
-            .align_items(Align::Center)
-            .push(header)
-            .push(item);
-
-        Scrollable::new(&mut self.plugin_scrollable_state)
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .padding(10)
-            .push(content)
+            .push(upgrade)
             .into()
     }
 }
--- src/gui/main_window.rs
+++ src/gui/main_window.rs
@@ -1,14 +1,19 @@
-use crate::core::Plugin;
+use crate::core::{Config, Plugin, Synchronizer};
 use crate::gui::elements::{ControlPanel, PluginPanel};
 use crate::gui::{assets::RING_BEARER, style};
-use iced::{Align, Application, Column, Command, Container, Element, Length, Sandbox, Settings};
+use iced::{
+    scrollable, Align, Application, Column, Command, Container, Element, Length, Row, Scrollable,
+    Settings, Text,
+};
 
 #[derive(Default, Debug, Clone)]
 pub struct MainWindow {
     control_panel: ControlPanel,
     plugin_panel: PluginPanel,
-
+    synchronizer: Synchronizer,
+    plugin_scrollable_state: scrollable::State,
     input_value: String,
+    all_plugins: Vec<Plugin>,
 }
 
 #[derive(Debug, Clone)]
@@ -20,8 +25,25 @@ pub enum Message {
 }
 
 impl MainWindow {
+    pub fn new(synchronizer: Synchronizer) -> Self {
+        Self {
+            control_panel: ControlPanel::default(),
+            plugin_panel: PluginPanel::default(),
+            synchronizer,
+            plugin_scrollable_state: scrollable::State::default(),
+            input_value: String::default(),
+            all_plugins: Vec::default(),
+        }
+    }
+
     pub async fn load() -> Self {
-        Self::default()
+        // TODO: Ka was genau hier passiert und warum ich das mit einem perform aufrufe. Eigentlich erstelle ich ja gerade zweimal die gleichen Objekte
+        let mut config = Config::default();
+        config.init_settings();
+        let synchronizer = Synchronizer::new(config);
+        synchronizer.create_plugins_db();
+
+        Self::new(synchronizer)
     }
 }
 
@@ -31,8 +53,13 @@ impl Application for MainWindow {
     type Flags = ();
 
     fn new(_flags: ()) -> (Self, Command<Message>) {
+        let mut config = Config::default();
+        config.init_settings();
+        let synchronizer = Synchronizer::new(config);
+        synchronizer.create_plugins_db();
+
         (
-            Self::default(),
+            Self::new(synchronizer),
             Command::perform(Self::load(), Message::Loaded),
         )
     }
@@ -42,19 +69,21 @@ impl Application for MainWindow {
     }
 
     fn update(&mut self, message: Message) -> Command<Message> {
-        // match message {
-        //     Message::RefreshPressed => {
-        //         println!("Refresh");
-        //     }
-        //     Message::UpdateAllPressed => {
-        //         println!("Update");
-        //     }
-        //     Message::InputChanged(state) => {
-        //         self.input_value = state;
-        //         println!("Changed");
-        //     }
-        //     Message::Loaded => {}
-        // }
+        match message {
+            Message::RefreshPressed => {
+                println!("Refresh");
+            }
+            Message::UpdateAllPressed => {
+                println!("Update");
+            }
+            Message::InputChanged(state) => {
+                self.input_value = state;
+                println!("Changed");
+            }
+            Message::Loaded(state) => {
+                self.all_plugins = state.synchronizer.get_plugins();
+            }
+        }
         Command::none()
     }
 
@@ -65,12 +94,19 @@ impl Application for MainWindow {
             ..
         } = self;
 
+        let mut plugins_scrollable = Scrollable::new(&mut self.plugin_scrollable_state);
+
+        for plugin in &mut self.all_plugins {
+            plugins_scrollable = plugins_scrollable.push(plugin.view());
+        }
+
         let content = Column::new()
             .width(Length::Fill)
             .spacing(10)
             .align_items(Align::Center)
             .push(control_panel.view())
-            .push(plugin_panel.view());
+            .push(plugin_panel.view())
+            .push(plugins_scrollable);
 
         Container::new(content)
             .width(Length::Fill)
