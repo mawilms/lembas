53ecd8bf2e27cdc724f919165afd183ef5e4a837
Marius Wilms <wilms.marius95@gmail.com>
2021-03-29 22:25:20 +0200

    Deleted unused file

--- Lembas Plugins.session.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE plugins (
-    id INTEGER PRIMARY KEY,
-    plugin_id INTEGER,
-    title TEXT,
-    current_version TEXT,
-    latest_version TEXT
-);
--- src/gui/elements/plugin_panel.rs
+++ src/gui/elements/plugin_panel.rs
@@ -1,24 +1,53 @@
 use crate::gui::main_window::Message;
-use iced::{Align, Element, Length, Row, Text};
+use iced::{scrollable, Align, Column, Container, Element, Length, Row, Scrollable, Text};
 
 #[derive(Default)]
-pub struct PluginPanel {}
+pub struct PluginPanel {
+    plugin_scrollable_state: scrollable::State,
+}
 
 impl PluginPanel {
-    pub fn view(&self) -> Element<Message> {
-        let plugin_name = Text::new("Plugin").width(Length::Fill);
-        let current_version = Text::new("Current Version");
-        let latest_version = Text::new("Latest version");
-        let upgrade = Text::new("Upgrade");
+    pub fn view(&mut self) -> Element<Message> {
+        let plugin_name = Text::new("Plugin").width(Length::FillPortion(5));
+        let current_version = Text::new("Current Version").width(Length::FillPortion(3));
+        let latest_version = Text::new("Latest version").width(Length::FillPortion(3));
+        let upgrade = Text::new("Upgrade").width(Length::FillPortion(2));
 
-        Row::new()
+        let header = Row::new()
             .width(Length::Fill)
             .align_items(Align::Center)
             .spacing(10)
             .push(plugin_name)
             .push(current_version)
             .push(latest_version)
-            .push(upgrade)
+            .push(upgrade);
+
+        let plugin_name = Text::new("TitanBar").width(Length::FillPortion(5));
+        let current_version = Text::new("v1.20.00").width(Length::FillPortion(3));
+        let latest_version = Text::new("v1.24.45").width(Length::FillPortion(3));
+        let upgrade = Text::new("Upgrade").width(Length::FillPortion(2));
+
+        let item = Row::new()
+            .width(Length::Fill)
+            .align_items(Align::Center)
+            .spacing(10)
+            .push(plugin_name)
+            .push(current_version)
+            .push(latest_version)
+            .push(upgrade);
+
+        let content = Column::new()
+            .width(Length::Fill)
+            .spacing(10)
+            .align_items(Align::Center)
+            .push(header)
+            .push(item);
+
+        Scrollable::new(&mut self.plugin_scrollable_state)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .padding(10)
+            .push(content)
             .into()
     }
 }
