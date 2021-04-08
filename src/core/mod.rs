78003a75fff2d3bbd70e0b2ff9ae4a3195328c8e
Marius Wilms <wilms.marius95@gmail.com>
2021-04-08 22:58:01 +0200

    Finished synchronizer

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -6,6 +6,4 @@ pub mod synchronizer;
 pub use config::Config;
 pub use installer::Installer;
 pub use plugin::Plugin;
-pub use synchronizer::{
-    create_plugins_db, get_installed_plugins, get_plugin, get_plugins, update_local_plugins,
-};
+pub use synchronizer::Synchronizer;
--- src/core/synchronizer.rs
+++ src/core/synchronizer.rs
@@ -4,27 +4,30 @@ use rusqlite::NO_PARAMS;
 use rusqlite::{params, Connection};
 use std::{collections::HashMap, error::Error};
 
-// Used to synchronize the local database with the remote plugin server
-#[tokio::main]
-pub async fn update_local_plugins() -> Result<(), Box<dyn Error>> {
-    let response = reqwest::get("https://young-hamlet-23901.herokuapp.com/plugins")
-        .await?
-        .json::<HashMap<String, Plugin>>()
-        .await?;
-    let mut remote_plugins: Vec<Plugin> = Vec::new();
-    for (_, element) in response {
-        remote_plugins.push(element);
+pub struct Synchronizer {}
+
+impl Synchronizer {
+    // Used to synchronize the local database with the remote plugin server
+    #[tokio::main]
+    pub async fn update_local_plugins() -> Result<(), Box<dyn Error>> {
+        let response = reqwest::get("https://young-hamlet-23901.herokuapp.com/plugins")
+            .await?
+            .json::<HashMap<String, Plugin>>()
+            .await?;
+        let mut remote_plugins: Vec<Plugin> = Vec::new();
+        for (_, element) in response {
+            remote_plugins.push(element);
+        }
+        Self::write_plugins(&remote_plugins)?;
+
+        Ok(())
     }
-    write_plugins(&remote_plugins)?;
 
-    Ok(())
-}
-
-// Creates the local database if it doesn't exist.
-pub fn create_plugins_db() {
-    let conn = Connection::open(&CONFIGURATION.db_file).unwrap();
-    conn.execute(
-        "
+    // Creates the local database if it doesn't exist.
+    pub fn create_plugins_db() {
+        let conn = Connection::open(&CONFIGURATION.db_file).unwrap();
+        conn.execute(
+            "
                 CREATE TABLE IF NOT EXISTS plugins (
                     id INTEGER PRIMARY KEY,
                     plugin_id INTEGER UNIQUE,
@@ -33,100 +36,134 @@ pub fn create_plugins_db() {
                     latest_version TEXT
                 );
         ",
-        NO_PARAMS,
-    )
-    .unwrap();
-}
-
-fn write_plugins(plugins: &[Plugin]) -> Result<(), Box<dyn Error>> {
-    for value in plugins {
-        insert_plugin(&value)?;
+            NO_PARAMS,
+        )
+        .unwrap();
     }
-    Ok(())
-}
 
-fn insert_plugin(plugin: &Plugin) -> Result<(), Box<dyn Error>> {
-    let conn = Connection::open(&CONFIGURATION.db_file).unwrap();
-    conn.execute(
+    fn write_plugins(plugins: &[Plugin]) -> Result<(), Box<dyn Error>> {
+        for value in plugins {
+            Self::insert_plugin(&value)?;
+        }
+        Ok(())
+    }
+
+    fn insert_plugin(plugin: &Plugin) -> Result<(), Box<dyn Error>> {
+        let existing_plugin = Self::get_plugin(&plugin.title);
+        if existing_plugin.len() == 1 {
+            Self::update_plugin(&plugin);
+        } else {
+            let conn = Connection::open(&CONFIGURATION.db_file)?;
+            conn.execute(
             "INSERT INTO plugins (plugin_id, title, current_version, latest_version) VALUES (?1, ?2, ?3, ?4) ON CONFLICT (plugin_id) DO UPDATE SET plugin_id=?1, title=?2, latest_version=?4;",
             params![plugin.plugin_id, plugin.title, "", plugin.latest_version],
         )?;
-    Ok(())
-}
-
-pub fn install_plugin(plugin: &Plugin) -> Result<(), Box<dyn Error>> {
-    let conn = Connection::open(&CONFIGURATION.db_file)?;
-    conn.execute(
-            "INSERT INTO plugins (plugin_id, title, current_version, latest_version) VALUES (?1, ?2, ?3, ?4) ON CONFLICT (plugin_id) DO UPDATE SET plugin_id=?1, title=?2, current_version=?3, latest_version=?4;",
-            params![plugin.plugin_id, plugin.title, plugin.latest_version, plugin.latest_version],
-        )?;
-    Ok(())
-}
-
-pub fn get_plugins() -> Vec<Plugin> {
-    let mut all_plugins: Vec<Plugin> = Vec::new();
-    let conn = Connection::open(&CONFIGURATION.db_file).unwrap();
-    let mut stmt = conn
-        .prepare("SELECT plugin_id, title, current_version, latest_version FROM plugins;")
-        .unwrap();
-    let plugin_iter = stmt
-        .query_map(params![], |row| {
-            Ok(Plugin {
-                plugin_id: row.get(0).unwrap(),
-                title: row.get(1).unwrap(),
-                current_version: row.get(2).unwrap(),
-                latest_version: row.get(3).unwrap(),
-            })
-        })
-        .unwrap();
-
-    for plugin in plugin_iter {
-        all_plugins.push(plugin.unwrap());
+        }
+        Ok(())
     }
-    all_plugins
-}
 
-pub fn get_installed_plugins() -> Vec<Plugin> {
-    let mut installed_plugins: Vec<Plugin> = Vec::new();
-    let conn = Connection::open(&CONFIGURATION.db_file).unwrap();
-    let mut stmt = conn
+    fn update_plugin(plugin: &Plugin) -> Result<(), Box<dyn Error>> {
+        let conn = Connection::open(&CONFIGURATION.db_file)?;
+        conn.execute(
+            "INSERT INTO plugins (plugin_id, title, current_version, latest_version) VALUES (?1, ?2, ?3, ?4) ON CONFLICT (plugin_id) DO UPDATE SET plugin_id=?1, title=?2, latest_version=?4;",
+            params![plugin.plugin_id, plugin.title, "", plugin.latest_version],
+        )?;
+        Ok(())
+    }
+
+    pub fn delete_plugin(plugin: &Plugin) -> Result<(), Box<dyn Error>> {
+        let conn = Connection::open(&CONFIGURATION.db_file)?;
+        conn.execute("DELETE FROM plugins WHERE title=?1;", params![plugin.title])?;
+        Ok(())
+    }
+
+    pub fn get_plugins() -> Vec<Plugin> {
+        let mut all_plugins: Vec<Plugin> = Vec::new();
+        let conn = Connection::open(&CONFIGURATION.db_file).unwrap();
+        let mut stmt = conn
+            .prepare("SELECT plugin_id, title, current_version, latest_version FROM plugins;")
+            .unwrap();
+        let plugin_iter = stmt
+            .query_map(params![], |row| {
+                Ok(Plugin {
+                    plugin_id: row.get(0).unwrap(),
+                    title: row.get(1).unwrap(),
+                    current_version: row.get(2).unwrap(),
+                    latest_version: row.get(3).unwrap(),
+                })
+            })
+            .unwrap();
+
+        for plugin in plugin_iter {
+            all_plugins.push(plugin.unwrap());
+        }
+        all_plugins
+    }
+
+    pub fn get_installed_plugins() -> Vec<Plugin> {
+        let mut installed_plugins: Vec<Plugin> = Vec::new();
+        let conn = Connection::open(&CONFIGURATION.db_file).unwrap();
+        let mut stmt = conn
             .prepare("SELECT plugin_id, title, current_version, latest_version FROM plugins WHERE current_version != '';")
             .unwrap();
-    let plugin_iter = stmt
-        .query_map(params![], |row| {
-            Ok(Plugin {
-                plugin_id: row.get(0).unwrap(),
-                title: row.get(1).unwrap(),
-                current_version: row.get(2).unwrap(),
-                latest_version: row.get(3).unwrap(),
+        let plugin_iter = stmt
+            .query_map(params![], |row| {
+                Ok(Plugin {
+                    plugin_id: row.get(0).unwrap(),
+                    title: row.get(1).unwrap(),
+                    current_version: row.get(2).unwrap(),
+                    latest_version: row.get(3).unwrap(),
+                })
             })
-        })
-        .unwrap();
+            .unwrap();
 
-    for plugin in plugin_iter {
-        installed_plugins.push(plugin.unwrap());
+        for plugin in plugin_iter {
+            installed_plugins.push(plugin.unwrap());
+        }
+        installed_plugins
     }
-    installed_plugins
-}
 
-pub fn get_plugin(name: &str) -> Vec<Plugin> {
-    let mut installed_plugins: Vec<Plugin> = Vec::new();
-    let conn = Connection::open(&CONFIGURATION.db_file).unwrap();
-    let mut stmt = conn
+    pub fn search_plugin(name: &str) -> Vec<Plugin> {
+        let mut installed_plugins: Vec<Plugin> = Vec::new();
+        let conn = Connection::open(&CONFIGURATION.db_file).unwrap();
+        let mut stmt = conn
             .prepare("SELECT plugin_id, title, current_version, latest_version FROM plugins WHERE LOWER(title) LIKE ?1;")
             .unwrap();
-    let plugin_iter = stmt
-        .query_map(params![format!("%{}%", name.to_lowercase())], |row| {
-            Ok(Plugin {
-                plugin_id: row.get(0).unwrap(),
-                title: row.get(1).unwrap(),
-                current_version: row.get(2).unwrap(),
-                latest_version: row.get(3).unwrap(),
+        let plugin_iter = stmt
+            .query_map(params![format!("%{}%", name.to_lowercase())], |row| {
+                Ok(Plugin {
+                    plugin_id: row.get(0).unwrap(),
+                    title: row.get(1).unwrap(),
+                    current_version: row.get(2).unwrap(),
+                    latest_version: row.get(3).unwrap(),
+                })
             })
-        })
-        .unwrap();
-    for plugin in plugin_iter {
-        installed_plugins.push(plugin.unwrap());
+            .unwrap();
+        for plugin in plugin_iter {
+            installed_plugins.push(plugin.unwrap());
+        }
+        installed_plugins
     }
-    installed_plugins
+
+    pub fn get_plugin(name: &str) -> Vec<Plugin> {
+        let mut installed_plugins: Vec<Plugin> = Vec::new();
+        let conn = Connection::open(&CONFIGURATION.db_file).unwrap();
+        let mut stmt = conn
+            .prepare("SELECT plugin_id, title, current_version, latest_version FROM plugins WHERE LOWER(title) = ?1 LIMIT 1;")
+            .unwrap();
+        let plugin_iter = stmt
+            .query_map(params![name.to_lowercase()], |row| {
+                Ok(Plugin {
+                    plugin_id: row.get(0).unwrap(),
+                    title: row.get(1).unwrap(),
+                    current_version: row.get(2).unwrap(),
+                    latest_version: row.get(3).unwrap(),
+                })
+            })
+            .unwrap();
+        for plugin in plugin_iter {
+            installed_plugins.push(plugin.unwrap());
+        }
+        installed_plugins
+    }
 }
