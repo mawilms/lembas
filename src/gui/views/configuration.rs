7bdd45e08490da8f54528f34ac6af2ca0081edb6
Marius Wilms <wilms.marius95@gmail.com>
2021-08-17 09:03:13 +0200

    Implemented feed url

--- src/gui/views/configuration.rs
+++ src/gui/views/configuration.rs
@@ -1,11 +1,13 @@
 use crate::{core::Config, gui::style};
-use iced::{Checkbox, Column, Container, Element, Length, Text};
+use iced::{Align, Checkbox, Column, Container, Element, Length, Row, Text, TextInput, text_input};
 
 #[derive(Debug, Clone)]
 pub struct Configuration {
     config: Config,
     description: String,
     backup: bool,
+    feed_url: text_input::State,
+    feed_url_value: String,
 }
 
 impl Configuration {
@@ -14,6 +16,8 @@ impl Configuration {
             config: config.clone(),
             description: "Enable Backup".to_string(),
             backup: config.application_settings.backup_enabled,
+            feed_url: text_input::State::default(),
+            feed_url_value: "".to_string(),
         }
     }
 }
@@ -21,6 +25,7 @@ impl Configuration {
 #[derive(Debug, Clone)]
 pub enum Message {
     BackupTriggered(bool),
+    FeedUrlChanged(String),
 }
 
 impl Configuration {
@@ -31,19 +36,41 @@ impl Configuration {
                 self.config.application_settings.backup_enabled = self.backup;
                 self.config.save_changes();
             }
+            Message::FeedUrlChanged(state) => {
+                self.feed_url_value = state;
+                self.config.save_changes();
+            }
         }
     }
 
-    pub fn view(&self) -> Element<Message> {
+    pub fn view(&mut self) -> Element<Message> {
         let about_text = Text::new("General");
 
         let checkbox = Checkbox::new(self.backup, &self.description, Message::BackupTriggered);
+
+        let feed_label = Text::new("Feed URL");
+
+        let feed_url = TextInput::new(
+            &mut self.feed_url,
+            "Feed Url",
+            &self.feed_url_value,
+            Message::FeedUrlChanged,
+        )
+        .padding(5);
+
+        let feed_row = Row::new()
+            .width(Length::Shrink)
+            .align_items(Align::Center)
+            .spacing(10)
+            .push(feed_label)
+            .push(feed_url);
 
         let content = Column::new()
             .width(Length::Fill)
             .spacing(10)
             .push(about_text)
-            .push(checkbox);
+            .push(checkbox)
+            .push(feed_row);
 
         Container::new(content)
             .padding(10)
