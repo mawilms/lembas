7f5cbb9ab4a61dcc1178d68587a827661619a4c0
Marius Wilms <info@mariuswilms.dev>
2023-11-27 21:41:15 +0100

    First visual prototyp without logic

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,79 +1,35 @@
 <script lang="ts">
-  import logo from './assets/images/logo-universal.png'
-  import {Greet} from '../wailsjs/go/main/App.js'
+    import {Greet} from '../wailsjs/go/main/App.js'
+    import Plugins from "./pages/Plugins.svelte";
 
-  let resultText: string = "Please enter your name below 👇"
-  let name: string
+    let resultText: string = "Please enter your name below 👇"
+    let name: string
 
-  function greet(): void {
-    Greet(name).then(result => resultText = result)
-  }
+    function greet(): void {
+        Greet(name).then(result => resultText = result)
+    }
 </script>
 
-<main>
-  <img alt="Wails logo" id="logo" src="{logo}">
-  <div class="result" id="result">{resultText}</div>
-  <div class="input-box" id="input">
-    <input autocomplete="off" bind:value={name} class="input" id="name" type="text"/>
-    <button class="btn" on:click={greet}>Greet</button>
-  </div>
+<main class="h-screen overflow-hidden">
+    <nav class="flex justify-between bg-light-brown py-4 px-8">
+        <div class="flex space-x-8 items-center">
+            <h1>Lembas</h1>
+            <ul class="flex border-solid border-2 border-primary rounded-xl overflow-hidden">
+                <li><a class="block text-primary active:bg-primary-transparent hover:bg-primary-transparent py-2 px-4"
+                       href="#">My Plugins</a></li>
+                <li><a class="block text-primary active:bg-primary-transparent hover:bg-primary-transparent py-2 px-4"
+                       href="#">Catalog</a></li>
+            </ul>
+        </div>
+        <div>
+            <ul class="flex space-x-4 border-solid border-2 border-primary rounded-xl overflow-hidden">
+                <li><a class="block text-primary  py-2 px-4" href="#">About</a></li>
+                <li><a class="block text-primary  py-2 px-4" href="#">Settings</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="px-8">
+        <Plugins/>
+    </div>
 </main>
-
-<style>
-
-  #logo {
-    display: block;
-    width: 50%;
-    height: 50%;
-    margin: auto;
-    padding: 10% 0 0;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: 100% 100%;
-    background-origin: content-box;
-  }
-
-  .result {
-    height: 20px;
-    line-height: 20px;
-    margin: 1.5rem auto;
-  }
-
-  .input-box .btn {
-    width: 60px;
-    height: 30px;
-    line-height: 30px;
-    border-radius: 3px;
-    border: none;
-    margin: 0 0 0 20px;
-    padding: 0 8px;
-    cursor: pointer;
-  }
-
-  .input-box .btn:hover {
-    background-image: linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%);
-    color: #333333;
-  }
-
-  .input-box .input {
-    border: none;
-    border-radius: 3px;
-    outline: none;
-    height: 30px;
-    line-height: 30px;
-    padding: 0 10px;
-    background-color: rgba(240, 240, 240, 1);
-    -webkit-font-smoothing: antialiased;
-  }
-
-  .input-box .input:hover {
-    border: none;
-    background-color: rgba(255, 255, 255, 1);
-  }
-
-  .input-box .input:focus {
-    border: none;
-    background-color: rgba(255, 255, 255, 1);
-  }
-
-</style>
---
+++ frontend/src/components/ProgressBar.svelte
@@ -0,0 +1,10 @@
+<script></script>
+
+<div class="flex flex-col my-8">
+<!--    <div class="w-1/4 bg-gray-200 rounded-full dark:bg-gray-700">-->
+<!--        <div class="bg-primary text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"-->
+<!--             style="width: 45%"> 45%-->
+<!--        </div>-->
+<!--    </div>-->
+<!--    <div><p>Installing plugin...</p></div>-->
+</div>
