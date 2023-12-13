9dc1f47d24cdfa7d8c445422fc29dfbfbb9a09fd
Marius Wilms <info@mariuswilms.dev>
2023-12-13 14:28:41 +0100

    fixed catalog scrolling

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,30 +1,31 @@
 <script lang="ts">
-    import "../app.css";
+	import '../app.css';
 </script>
 
-<main class="h-screen overflow-hidden">
-    <nav class="flex justify-between bg-light-brown py-4 px-8">
-        <div class="flex space-x-8 items-center">
-            <h1>Lembas</h1>
-            <ul class="flex border-solid border-2 border-primary rounded-xl overflow-hidden">
-                <li>
-                    <a class="block text-primary active:bg-primary-transparent hover:bg-primary-transparent py-2 px-4"
-                       href="/">My Plugins</a></li>
-                <li>
-                    <a class="block text-primary active:bg-primary-transparent hover:bg-primary-transparent py-2 px-4"
-                       href="/catalog">Catalog</a></li>
-            </ul>
-        </div>
-        <div>
-            <ul class="flex space-x-4 border-solid border-2 border-primary rounded-xl overflow-hidden">
-                <li><a class="block text-primary  py-2 px-4" href="#">About</a></li>
-                <li><a class="block text-primary  py-2 px-4" href="/settings">Settings</a></li>
-            </ul>
-        </div>
-    </nav>
+<main class="h-full">
+	<nav class="flex justify-between items-center bg-light-brown py-4 px-8 h-20">
+		<div class="flex space-x-8 items-center">
+			<h1>Lembas</h1>
+			<ul class="flex border-solid border-2 border-primary rounded-xl overflow-hidden">
+				<li>
+					<a class="block text-primary active:bg-primary-transparent hover:bg-primary-transparent py-2 px-4"
+						 href="/">My Plugins</a></li>
+				<li>
+					<a class="block text-primary active:bg-primary-transparent hover:bg-primary-transparent py-2 px-4"
+						 href="/catalog">Catalog</a></li>
+			</ul>
+		</div>
+		<div>
+			<ul class="flex space-x-4 border-solid border-2 border-primary rounded-xl overflow-hidden">
+				<li><a class="block text-primary  py-2 px-4" href="#">About</a></li>
+				<li><a class="block text-primary  py-2 px-4" href="/settings">Settings</a></li>
+			</ul>
+		</div>
+	</nav>
 
-    <div class="px-8">
-        <slot />
-    </div>
+	<div class="absolute mt-20 top-0 right-0 left-0 bottom-0 p-4">
+		<slot />
+	</div>
 
 </main>
+<!--absolute left-0 top-0 right-0 bottom-0 mt-20 p-4 overflow-hidden-->
--- frontend/src/routes/catalog/+page.svelte
+++ frontend/src/routes/catalog/+page.svelte
@@ -1,76 +1,86 @@
 <script lang="ts">
-    import "../../app.css";
-    import {FetchRemotePlugins, InstallPlugin} from "$lib/wailsjs/go/main/App";
-    import {BrowserOpenURL} from "$lib/wailsjs/runtime/runtime"
-    import {RemotePlugin} from "$lib/models/remotePlugin";
+	import '../../app.css';
+	import { FetchRemotePlugins, InstallPlugin } from '$lib/wailsjs/go/main/App';
+	import { BrowserOpenURL } from '$lib/wailsjs/runtime/runtime';
+	import { RemotePlugin } from '$lib/models/remotePlugin';
 
-    let plugins: RemotePlugin[] = []
+	let plugins: RemotePlugin[] = [];
 
-    FetchRemotePlugins().then(result => {
-        let tmpArray: RemotePlugin[] = []
-        for (let i = 0; i < result.length; i++) {
-            const element = result[i]
-            const time = new Date(element.UpdatedTimestamp * 1000).toLocaleDateString()
-            const infoUrl = `https://www.lotrointerface.com/downloads/info${element.Id}-${element.Name}.html`
+	FetchRemotePlugins().then(result => {
+		let tmpArray: RemotePlugin[] = [];
+		for (let i = 0; i < result.length; i++) {
+			const element = result[i];
+			const time = new Date(element.UpdatedTimestamp * 1000).toLocaleDateString();
+			const infoUrl = `https://www.lotrointerface.com/downloads/info${element.Id}-${element.Name}.html`;
 
-            tmpArray.push(new RemotePlugin(element.Id, element.Name, element.Author, element.Version, time, element.Downloads, element.Category, element.Description, element.FileName, infoUrl, element.Url))
-        }
+			tmpArray.push(new RemotePlugin(element.Id, element.Name, element.Author, element.Version, time, element.Downloads, element.Category, element.Description, element.FileName, infoUrl, element.Url));
+		}
+		const labelDocument = document.getElementById("plugin-labels")
+		const pluginListDocument = document.getElementById("plugin-list")!
 
-        plugins = tmpArray
-    })
+		labelDocument.style.paddingRight = pluginListDocument.offsetWidth - pluginListDocument.clientWidth + "px"
+		console.log(pluginListDocument.offsetWidth - pluginListDocument.clientWidth + "px")
 
-    const toggleDetails = (index: number) => {
-        let element = document.getElementById(`details-${index}`)!
-        if (element.classList.contains("hidden")) {
-            element.classList.remove("hidden")
-        } else {
-            element.classList.add("hidden")
-        }
-    }
+		plugins = tmpArray;
+	});
 
-    const openUrl = (url: string) => {
-        BrowserOpenURL(url)
-    }
+	const toggleDetails = (index: number) => {
+		let element = document.getElementById(`details-${index}`)!;
+		if (element.classList.contains('hidden')) {
+			element.classList.remove('hidden');
+		} else {
+			element.classList.add('hidden');
+		}
+	};
+
+	const openUrl = (url: string) => {
+		BrowserOpenURL(url);
+	};
+
+
 </script>
 
-<div class="text-left my-4 space-y-4">
-    <div class="flex mx-2 space-x-4">
-        <p class="w-1/3">Name</p>
-        <div class="flex w-2/3">
-        <p class="w-1/5">Version</p>
-        <p class="w-1/5">Author</p>
-        <p class="w-1/5">Downloads</p>
-        <p class="w-1/5">Latest Release</p>
-        <p class="w-1/5 text-center">Status</p>
-        </div>
-    </div>
+<div class="h-full text-left space-y-4 overflow-hidden">
+	<div id="plugin-labels" class="flex mx-2 space-x-4">
+		<p class="w-1/3">Name</p>
+		<div class="flex w-2/3">
+			<p class="w-1/5">Version</p>
+			<p class="w-1/5">Author</p>
+			<p class="w-1/5">Downloads</p>
+			<p class="w-1/5">Latest Release</p>
+			<p class="w-1/5 text-center">Status</p>
+		</div>
+	</div>
 
-    <div>
-        <ul class="space-y-2 h-96">
-            {#each plugins as plugin, index}
-                <li id="plugin-{index}" class="bg-light-brown p-2">
-                    <div class="flex space-x-4" on:click={() => toggleDetails(index)}>
-                        <p class="w-1/3">{plugin.name}</p>
-                        <div class="flex w-2/3">
-                        <p class="w-1/5">{plugin.version}</p>
-                        <p class="w-1/5">{plugin.author}</p>
-                        <p class="w-1/5">{plugin.totalDownloads}</p>
-                        <p class="w-1/5">{plugin.lastUpdated}</p>
-                        <p class="w-1/5 text-center text-gold">Installed</p>
-                        </div>
-                    </div>
-                    <div id="details-{index}" class="hidden mt-2 p-4 bg-dark-brown">
-                        <p>{plugin.description}</p>
-                        <div class="flex justify-between mt-4">
-                            <button class="bg-primary text-dark-brown py-1 px-2 rounded-xl overflow-hidden" on:click={() => openUrl(plugin.url)}>Open website</button>
-                            <div class="flex space-x-4">
-                                <button class="bg-primary text-dark-brown py-1 px-2 rounded-xl overflow-hidden" on:click={() => InstallPlugin(plugin.downloadUrl)}>Install/Update</button>
-                                <button class="bg-primary text-dark-brown py-1 px-2 rounded-xl overflow-hidden">Remove</button>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-            {/each}
-        </ul>
-    </div>
+	<ul id="plugin-list" class="space-y-2 h-full overflow-y-scroll">
+		{#each plugins as plugin, index}
+			<li id="plugin-{index}" class="block bg-light-brown p-2">
+				<div class="flex space-x-4" on:click={() => toggleDetails(index)}>
+					<p class="w-1/3">{plugin.name}</p>
+					<div class="flex w-2/3">
+						<p class="w-1/5">{plugin.version}</p>
+						<p class="w-1/5">{plugin.author}</p>
+						<p class="w-1/5">{plugin.totalDownloads}</p>
+						<p class="w-1/5">{plugin.lastUpdated}</p>
+						<p class="w-1/5 text-center text-gold">Installed</p>
+					</div>
+				</div>
+				<div id="details-{index}" class="hidden mt-2 p-4 bg-dark-brown">
+					<p>{plugin.description}</p>
+					<div class="flex justify-between mt-4">
+						<button class="bg-primary text-dark-brown py-1 px-2 rounded-xl overflow-hidden"
+										on:click={() => openUrl(plugin.url)}>Open website
+						</button>
+						<div class="flex space-x-4">
+							<button class="bg-primary text-dark-brown py-1 px-2 rounded-xl overflow-hidden"
+											on:click={() => InstallPlugin(plugin.downloadUrl)}>Install/Update
+							</button>
+							<button class="bg-primary text-dark-brown py-1 px-2 rounded-xl overflow-hidden">Remove</button>
+						</div>
+					</div>
+				</div>
+			</li>
+		{/each}
+	</ul>
+
 </div>
