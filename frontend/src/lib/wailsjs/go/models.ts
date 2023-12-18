37ac96ba07b5dbaf6c6abb6505d35e196e1952d7
Marius Wilms <info@mariuswilms.dev>
2023-12-18 17:53:37 +0100

    Added sorting

---
+++ frontend/src/lib/wailsjs/go/models.ts
@@ -0,0 +1,21 @@
+export namespace settings {
+
+	export class Settings {
+		pluginPath: string;
+		dataDirectory: string;
+		infoUrl: string;
+
+		constructor(source: any = {}) {
+			if ('string' === typeof source) source = JSON.parse(source);
+			this.pluginPath = source['pluginPath'];
+			this.dataDirectory = source['dataDirectory'];
+			this.infoUrl = source['infoUrl'];
+		}
+
+		static createFrom(source: any = {}) {
+			return new Settings(source);
+		}
+	}
+
+}
+
