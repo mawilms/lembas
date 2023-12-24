36f4f354f9e67577db50430cbdf13b61e53abeaa
Marius Wilms <wilms.marius95@gmail.com>
2023-12-24 23:45:46 +0100

    Fixed references

--- app.go
+++ app.go
@@ -37,13 +37,13 @@ func (a *App) startup(ctx context.Context) {
 }
 
 func (a *App) SearchLocal(input string) []entities.LocalPluginEntity {
-	plugins := processes.SearchLocal(input, a.localPlugins)
+	plugins := a.process.SearchLocal(input, a.localPlugins)
 
 	return plugins
 }
 
 func (a *App) SearchRemote(input string) []entities.RemotePluginEntity {
-	plugins := processes.SearchRemote(input, a.remotePlugins)
+	plugins := a.process.SearchRemote(input, a.remotePlugins)
 
 	return plugins
 }
@@ -68,7 +68,7 @@ func (a *App) GetSettings() settings.Settings {
 func (a *App) InstallPlugin(url string) []entities.RemotePluginEntity {
 	a.logger.Info("trying to install plugin", slog.String("url", url))
 
-	plugins, err := processes.InstallPlugin(a.datastore, url, a.settings.PluginDirectory, a.remotePlugins)
+	plugins, err := a.process.InstallPlugin(a.datastore, url, a.settings.PluginDirectory, a.remotePlugins)
 	if err != nil {
 		a.logger.Error("failed to install plugin", slog.String("url", url), slog.String("error", err.Error()))
 		return make([]entities.RemotePluginEntity, 0)
@@ -83,7 +83,7 @@ func (a *App) InstallPlugin(url string) []entities.RemotePluginEntity {
 func (a *App) DeletePlugin(name, author string) []entities.LocalPluginEntity {
 	a.logger.Info("trying to delete plugin", slog.String("name", name), slog.String("author", author))
 
-	plugins, err := processes.DeletePlugin(a.datastore, name, author, a.settings.PluginDirectory)
+	plugins, err := a.process.DeletePlugin(a.datastore, name, author, a.settings.PluginDirectory)
 	if err != nil {
 		a.logger.Error("failed to delete plugin", slog.String("name", name), slog.String("author", author), slog.String("error", err.Error()))
 		return make([]entities.LocalPluginEntity, 0)
@@ -103,7 +103,7 @@ func (a *App) UpdatePlugins(plugins any) {
 func (a *App) GetInstalledPlugins() []entities.LocalPluginEntity {
 	a.logger.Info("loading installed plugins")
 
-	plugins, err := processes.GetInstalledPlugins(a.datastore)
+	plugins, err := a.process.GetInstalledPlugins(a.datastore)
 	if err != nil {
 		a.logger.Error("failed to get installed plugins", slog.String("error", err.Error()))
 		return make([]entities.LocalPluginEntity, 0)
@@ -118,7 +118,7 @@ func (a *App) GetInstalledPlugins() []entities.LocalPluginEntity {
 func (a *App) GetRemotePlugins() []entities.RemotePluginEntity {
 	a.logger.Info("loading remote plugins")
 
-	plugins, err := processes.GetRemotePlugins(a.settings.InfoUrl, a.localPlugins)
+	plugins, err := a.process.GetRemotePlugins(a.settings.InfoUrl, a.localPlugins)
 	if err != nil {
 		a.logger.Error("failed to get fetch remote plugins", slog.String("feed url", a.settings.InfoUrl), slog.String("error", err.Error()))
 		return make([]entities.RemotePluginEntity, 0)
