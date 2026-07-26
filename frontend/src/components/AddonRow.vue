a534a327157807217ab1524ac5a39ec605c55940
Marius Wilms <wilms.marius95@gmail.com>
2026-07-26 12:19:20 +0200

    Increased letter in browser view

--- frontend/src/components/AddonRow.vue
+++ frontend/src/components/AddonRow.vue
@@ -19,7 +19,7 @@ const openPluginPage = (id: number) => {
         class="flex gap-4 py-4 items-start bg-light-brown hover:bg-light-brown-hover p-4 select-none"
     >
         <div
-            class="w-16 h-16 rounded-lg shrink-0 flex items-center justify-center text-2xl font-bold bg-secondary"
+            class="flex items-center justify-center w-22 h-full rounded-lg text-2xl font-bold bg-secondary"
         >
             {{ extractFirstLetter(props.addon.Name) }}
         </div>
