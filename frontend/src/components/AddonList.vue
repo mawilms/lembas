3ac1365df6658cd97b8ca05959f8ee83d0ecd07f
Marius Wilms <wilms.marius95@gmail.com>
2026-07-27 21:47:14 +0200

    Use list virtualization

--- frontend/src/components/AddonList.vue
+++ frontend/src/components/AddonList.vue
@@ -15,12 +15,19 @@ const resetRow = () => {
 </script>
 
 <template>
-    <ul class="flex flex-col flex-1 gap-4 overflow-y-auto p-4">
+    <UScrollArea
+        v-slot="{ item, index }"
+        :items="props.addons"
+        orientation="vertical"
+        :virtualize="{
+            gap: 16,
+        }"
+        class="p-4"
+    >
         <AddonRow
             class="border cursor-pointer"
-            v-for="(addon, index) in props.addons"
-            :key="addon.Id"
-            :addon="addon"
+            :key="index"
+            :addon="item"
             :class="{
                 'border-primary': activeRow === index,
                 'border-transparent': activeRow !== index,
@@ -28,14 +35,14 @@ const resetRow = () => {
             @click="activeRow = index"
         >
             <template v-slot:status>
-                <div v-if="addon.Type === 'remote'">
+                <div v-if="item.Type === 'remote'">
                     <button
-                        v-if="addon.HasUpdate"
+                        v-if="item.HasUpdate"
                         class="bg-primary hover:bg-gold text-sm py-1 px-2 rounded cursor-pointer"
                     >
                         Update
                     </button>
-                    <p v-else-if="addon.IsInstalled">Installed</p>
+                    <p v-else-if="item.IsInstalled">Installed</p>
                     <button
                         v-else
                         class="bg-primary hover:bg-gold text-sm py-1 px-2 rounded cursor-pointer"
@@ -46,7 +53,7 @@ const resetRow = () => {
                 <div v-else>
                     <button
                         class="bg-primary hover: text-sm py-1 px-2 rounded cursor-pointer"
-                        v-if="addon.HasUpdate"
+                        v-if="item.HasUpdate"
                     >
                         Update
                     </button>
@@ -56,14 +63,64 @@ const resetRow = () => {
 
             <template v-slot:center>
                 <div class="flex min-w-0 flex-1 text-xs text-gray-300">
-                    <span v-if="addon.Type === 'local'" class="min-w-0 truncate">{{
-                        addon.ArchiveName
+                    <span v-if="item.Type === 'local'" class="min-w-0 truncate">{{
+                        item.ArchiveName
                     }}</span>
-                    <span v-else class="min-w-0 flex-1 truncate">{{ addon.Description }}</span>
+                    <span v-else class="min-w-0 flex-1 truncate">{{ item.Description }}</span>
                 </div>
             </template>
         </AddonRow>
-    </ul>
+    </UScrollArea>
+
+    <!--    <ul class="flex flex-col flex-1 gap-4 overflow-y-auto p-4">-->
+    <!--        <AddonRow-->
+    <!--            class="border cursor-pointer"-->
+    <!--            v-for="(addon, index) in props.addons"-->
+    <!--            :key="addon.Id"-->
+    <!--            :addon="addon"-->
+    <!--            :class="{-->
+    <!--                'border-primary': activeRow === index,-->
+    <!--                'border-transparent': activeRow !== index,-->
+    <!--            }"-->
+    <!--            @click="activeRow = index"-->
+    <!--        >-->
+    <!--            <template v-slot:status>-->
+    <!--                <div v-if="addon.Type === 'remote'">-->
+    <!--                    <button-->
+    <!--                        v-if="addon.HasUpdate"-->
+    <!--                        class="bg-primary hover:bg-gold text-sm py-1 px-2 rounded cursor-pointer"-->
+    <!--                    >-->
+    <!--                        Update-->
+    <!--                    </button>-->
+    <!--                    <p v-else-if="addon.IsInstalled">Installed</p>-->
+    <!--                    <button-->
+    <!--                        v-else-->
+    <!--                        class="bg-primary hover:bg-gold text-sm py-1 px-2 rounded cursor-pointer"-->
+    <!--                    >-->
+    <!--                        Install-->
+    <!--                    </button>-->
+    <!--                </div>-->
+    <!--                <div v-else>-->
+    <!--                    <button-->
+    <!--                        class="bg-primary hover: text-sm py-1 px-2 rounded cursor-pointer"-->
+    <!--                        v-if="addon.HasUpdate"-->
+    <!--                    >-->
+    <!--                        Update-->
+    <!--                    </button>-->
+    <!--                    <span v-else>Up to date</span>-->
+    <!--                </div>-->
+    <!--            </template>-->
+
+    <!--            <template v-slot:center>-->
+    <!--                <div class="flex min-w-0 flex-1 text-xs text-gray-300">-->
+    <!--                    <span v-if="addon.Type === 'local'" class="min-w-0 truncate">{{-->
+    <!--                        addon.ArchiveName-->
+    <!--                    }}</span>-->
+    <!--                    <span v-else class="min-w-0 flex-1 truncate">{{ addon.Description }}</span>-->
+    <!--                </div>-->
+    <!--            </template>-->
+    <!--        </AddonRow>-->
+    <!--    </ul>-->
 
     <SelectPopover :activeRow="activeRow" @reset-row="resetRow" />
 </template>
