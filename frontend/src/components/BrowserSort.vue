2267d556542f5664915263718a9f97980d8f0aa3
Marius Wilms <wilms.marius95@gmail.com>
2026-08-07 15:09:49 +0200

    Styled sorting

--- frontend/src/components/BrowserSort.vue
+++ frontend/src/components/BrowserSort.vue
@@ -22,13 +22,18 @@ function toggle(value: string) {
 
         <template #content>
             <div class="flex flex-col gap-4 bg-light-brown-hover p-4 select-none text-sm">
-                <UCollapsible class="flex flex-col w-60">
+                <UCollapsible class="flex flex-col w-60 space-y-2">
                     <UCheckbox
                         v-for="c in criteria"
                         :key="c"
                         :label="c"
                         :model-value="selectedCriteria === c"
                         @update:model-value="() => toggle(c)"
+                        :size="'sm'"
+                        :ui="{
+                            label: 'font-normal',
+                            base: 'bg-gray-300',
+                        }"
                     />
                 </UCollapsible>
             </div>
